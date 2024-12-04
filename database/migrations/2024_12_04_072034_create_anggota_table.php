dd421ed6a4eca5bfc5a9a0b1e0b9b82e6d47a669
Tonni Ramdani <tonniramdani@gmail.com>
2024-12-04 14:49:03 +0700

    membuat migrasi

---
+++ database/migrations/2024_12_04_072034_create_anggota_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggota', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('telepon');
+            $table->text('alamat');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggota');
+    }
+};
